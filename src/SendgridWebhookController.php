462a910fc0a0fe5d28a64a58b320b3a5d7128aed
riasvdv <rias@spatie.be>
2022-11-09 10:58:33 +0000

    Update split workflow

--- src/SendgridWebhookController.php
+++ src/SendgridWebhookController.php
@@ -3,15 +3,35 @@
 namespace Spatie\MailcoachSendgridFeedback;
 
 use Illuminate\Http\Request;
+use Spatie\Mailcoach\Domain\Shared\Traits\UsesMailcoachModels;
 
 class SendgridWebhookController
 {
+    use UsesMailcoachModels;
+
     public function __invoke(Request $request)
     {
+        $this->registerMailerConfig($request->route('mailer'));
+
         $webhookConfig = SendgridWebhookConfig::get();
 
         (new WebhookProcessor($request, $webhookConfig))->process();
 
         return response()->json(['message' => 'ok']);
+    }
+
+    public function registerMailerConfig(?string $mailer): void
+    {
+        if (! $mailer) {
+            return;
+        }
+
+        $mailer = cache()->remember(
+            "mailcoach-mailer-{$mailer}",
+            now()->addMinute(),
+            fn () => self::getMailerClass()::findByConfigKeyName($mailer),
+        );
+
+        $mailer?->registerConfigValues();
     }
 }
